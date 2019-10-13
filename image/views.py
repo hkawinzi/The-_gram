1e6bf246808d939e19879225c015df2ed0da3161
happiness <happinesskawinzi@gmail.com>
2019-10-13 07:04:16 +0300

    creating a form instance

--- image/views.py
+++ image/views.py
@@ -7,4 +7,4 @@ from .forms import LoginForms
 def get-name(request):
     #post request that processes the data from the form
     if request.method == 'POST':
-        
+        form = LoginForms(request.POST)
