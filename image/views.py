7210a46310fa3974bd160c673d79b10f6c2744a8
happiness <happinesskawinzi@gmail.com>
2019-10-13 07:05:30 +0300

    validating the form

--- image/views.py
+++ image/views.py
@@ -8,3 +8,5 @@ def get-name(request):
     #post request that processes the data from the form
     if request.method == 'POST':
         form = LoginForms(request.POST)
+        #validation
+        if form.is_valid()
