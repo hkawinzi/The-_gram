9c81221455e74c8f328af5ef4edd9e62eacc097a
happiness <happinesskawinzi@gmail.com>
2019-10-14 21:25:55 +0300

    creating a profile class

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
@@ -16,3 +17,10 @@ class Image(models.Model):
     def delete_image(self):
         image = Image.objects.all().delete()
         return image
+
+class Profile(models.Model):
+    # creating a relationship with an existing user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to= 'profile_pic')
+
+    
